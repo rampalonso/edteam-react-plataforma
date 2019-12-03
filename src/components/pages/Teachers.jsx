87f7fbb6c2dc65ab0fa69faa951257e45c5d5083
Alonso Moreno <alonso.moreno@gdpteam.com>
2019-12-03 15:09:33 -0500

    Agregando Banner component

--- src/components/pages/Teachers.jsx
+++ src/components/pages/Teachers.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Banner } from 'components/organisms'
 
 const Teachers = () => (
-  <div className="ed-grid">
-    <h1>Profesores</h1>
-  </div>
+  <Banner
+    color="third-color"
+    image={{
+      src: 'https://images.unsplash.com/photo-1523050854058-8df90110c9f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+      alt: 'Banner profesores'
+    }}
+    title="Nuestro profesores"
+    subtitle="Este panel docente esta altamente calificado para guiarte en tu educacion"
+  />
 )
 
 export default Teachers;
